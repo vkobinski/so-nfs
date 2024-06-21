4100d7a27815d753ae34a25901dedabe426c9341
vkobinski <victorkobinski@outlook.com>
2024-06-20 22:17:04 -0300

    First

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::{fmt::format, fs::File, io::Write, process::Command, time::Instant};
+
+fn main() {
+    for i in 0..8 {
+        let path = format!("time{}.txt", i + 1);
+        let mut file = File::create(path).unwrap();
+
+        let _ = file.write(format!("File {}\n", i).as_bytes());
+
+        for x in 0..10 {
+            let now = Instant::now();
+            let _ = Command::new("cmd")
+                .args([
+                    "/C",
+                    format!("scp root@154.56.43.220:~/so/{}.txt .", i).as_str(),
+                ])
+                .output()
+                .expect("failed to execute process");
+
+            let elapsed = now.elapsed();
+
+            let _ = file.write(format!("\tElapsed {} ms\n", elapsed.as_millis()).as_bytes());
+
+            let _ = Command::new("cmd")
+                .args(["/C", format!("rm {}.txt .", i).as_str()])
+                .output()
+                .expect("failed to execute process");
+        }
+    }
+}
